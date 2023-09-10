b71ccacaeb3f4b8184fccfb709d3817f12e56e87
Baek Gi Chan <laphayen@gmail.com>
2023-09-10 13:25:31 +0900

    Update mm.c

--- mm.c
+++ mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "team 7",
     /* First member's full name */
-    "Harry Bovik",
+    "Gichan Baek",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "laphayen@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
@@ -51,7 +51,7 @@ team_t team = {
 #define DSIZE 8
 
 // 힙을 확장 시 지정 - 4바이트
-#define DHUNKSIZE (1<<12)
+#define CHUNKSIZE (1<<12)
 
 // 최대값 가져온다.
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
@@ -81,11 +81,12 @@ team_t team = {
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 // 매크로 정의 완료
 
-
-// 할당기 자체는 사용자가 자신의 응용에서 컴파일하고 링크할 수 있는 소스 파일에 포함한다.
-extern int mm_init(void);
-extern void *mm_malloc(size_t size);
-extern void mm_free(void *ptr);
+// 전역 힙 변수 및 함수 선언
+static void *heap_listp;
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
 
 /* 
  * mm_init - initialize the malloc package.
@@ -99,8 +100,77 @@ extern void mm_free(void *ptr);
 // 할당기 자체는 사용자 자신의 응용에서 컴파일하고 링크할 수 있는 소스파일(mm.c)에 포함된다.
 // 할당기는 세 개의 함수를 응용 프로그램에 알려준다.
 int mm_init(void)
-{
+{   
+    // mem_sbrk: 힙 영역을 incr(0이 아닌 양수) bytes 만큼 확장하고, 새로 할당된 힙 영역의 첫번째 byte를 가리키는 제네릭 포인터를 리턴함
+    /* 비어있는 heap을 만든다.*/
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1){
+        return -1;
+    };
+        
+    PUT(heap_listp, 0);                             // Alignment padding
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    // Prologue header 
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));     // Prologue footer
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));          // Epilogue header
+    heap_listp += (2*WSIZE); 
+
+    // 두 가지 다른 경우에 호출된다.
+    // (1) 힙이 초기화 될때 (2) mm_malloc이 적당한 맞춤fit을 찾지 못했을 때
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL){
+        return -1;
+    }
+
     return 0;
+}
+
+static void *extend_heap(size_t words)
+{
+    // 요청한 크기를 인접 2워드의 배수(8바이트)로 반올림하여, 그 후에 추가적인 힙 공간 요청
+    char *bp;
+    size_t size;
+    // 요청한 크기를 2워드의 배수로 반올림하고 추가 힙 공간을 요청함
+    size = (words %2) ? (words+1)*WSIZE : words * WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+
+    PUT(HDRP(bp), PACK(size, 0));  //free 블록의 header
+    PUT(FTRP(bp), PACK(size, 0));  //free 블록의 footer
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // new epilogue header
+
+    return coalesce(bp);
+};
+
+// 할당된 블록을 합칠 수 있는 경우 4가지에 따라 메모리 연결
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc){              // Case 1 : 현재만 반환시
+        return bp;
+    }
+    else if(prev_alloc && !next_alloc){         // Case 2 : 다음 블록과 병합
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if(!prev_alloc && next_alloc){         // Case 3 : 이전 블록과 병합
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+        bp = PREV_BLKP(bp);
+    }
+    else{                                       // Case 4 : 이전 블록과 다음 블록 병합
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        // PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        // PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        // bp = PREV_BLKP(bp);
+        bp = PREV_BLKP(bp);
+        PUT(HDRP((bp)), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    return bp;
 }
 
 /* 
@@ -109,14 +179,35 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    size_t asize;
+    size_t extendsize;
+    char *bp;
+
+    if (size == 0){
+        return NULL;
     }
+    // size를 바탕으로 헤더와 푸터의 공간 확보
+    // 8바이트는 정렬조건을 만족하기 위해
+    // 추가 8바이트는 헤더와 푸터 오버헤드를 위해서 확보
+    if (size <= DSIZE){
+        asize = 2*DSIZE;
+    }else{
+        asize = DSIZE*((size+(DSIZE) + (DSIZE-1)) / DSIZE);
+    }
+
+    // 가용 블록을 가용리스트에서 검색하고 할당기는 요청한 블록을 배치한다.
+    if((bp = find_fit(asize)) !=NULL){
+        place(bp, asize);
+        return bp;
+    }
+
+    //맞는 블록을 찾기 못한다면 새로운 가용 블록으로 확장하고 배치한다.
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+
+    return bp;
 }
 
 /*
@@ -124,7 +215,45 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
-    
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
+}
+
+static void *find_fit(size_t asize){
+    // 적절한 가용 블록을 검색하고 가용블록의 주소를 반환한다
+    //first fit 검색을 수행한다. -> 리스트 처음부터 탐색하여 가용블록 찾기
+    void *bp;
+    //헤더의 사이즈가 0보다 크다. -> 에필로그까지 탐색한다.
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) >0; bp = NEXT_BLKP(bp)){
+        if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+            return bp;
+        }
+    }
+    return NULL;
+}
+
+// 
+static void place(void *bp, size_t asize){
+    // 맞는 블록을 찾으면 요청한 블록을 배치하고 초과부분을 분할한다.
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if((csize - asize) >= (2*DSIZE)){
+        //가용 블록에 사이즈 - 요청한 블록의 사이즈 각 더블워드*2 크거나 같을때
+        //요청 블록을 넣고 남은 사이즈는 가용 블록으로 분할
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+    }else{
+        //할당하고 남은 블록이 더블워드*2보다 작다며 나누지 않고 할당
+        // 남은 블록이 더블워드*2보다 작은 경우는 데이터를 담을 수 없음
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
 }
 
 /*
